9a6dd931c35faea9e35f463de440653b1f88b3a3
James Secor <james.secor@gmail.com>
2021-11-22 20:34:41 -0500

    Added admin check for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate(auth_options)
-    if resource.present?
+    if resource.present? && resource.admin?
       sign_in(resource_name, resource)
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
